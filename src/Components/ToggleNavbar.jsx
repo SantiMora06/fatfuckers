3a2d9c687753a677c20f9bd75569265e327511df
Santi Mera <santigomero@gmail.com>
2024-07-05 19:20:02 +0300

    Toggle

--- src/Components/ToggleNavbar.jsx
+++ src/Components/ToggleNavbar.jsx
@@ -1,15 +1,48 @@
+import { Menu, Button, Text, rem } from '@mantine/core';
+
+
 const ToggleNavbar = () => {
     return ( 
-        <ul>
-            <li>Biceps</li>
-            <li>Triceps</li>
-            <li>Chest</li>
-            <li>Back</li>
-            <li>Shoulders</li>
-            <li>Abbs & Core</li>
-            <li>Quadriceps</li>
-            <li>Calves</li>
-        </ul>
+        <section className='Toggle'>
+        <Menu shadow="md" width={1000}>
+      <Menu.Target>
+        <Button>Exercises</Button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>Upper Body</Menu.Label>
+        <Menu.Item>
+          Biceps
+        </Menu.Item>
+        <Menu.Item>
+          Triceps
+        </Menu.Item>
+        <Menu.Item>
+          Chest
+        </Menu.Item>
+        <Menu.Item>
+          Back
+        </Menu.Item>
+        <Menu.Item>
+          Shoulders
+        </Menu.Item>
+       
+
+        <Menu.Divider />
+
+        <Menu.Label>Lower Body</Menu.Label>
+        <Menu.Item>
+          Quads
+        </Menu.Item>
+        <Menu.Item>
+          Femoral
+        </Menu.Item>
+        <Menu.Item>
+          Calves
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+    </section>
      );
 }
  
