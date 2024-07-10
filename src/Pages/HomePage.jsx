db5d03a0fc0142edad51d2a23e35cd8f27011ca0
Omid Jahromi <omid.esmaeelipoor@gmail.com>
2024-07-10 04:36:49 +0200

    Resolved merge conflict in HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,11 +6,8 @@ import AboutUs from "./AboutUs";
 import Workouts from "./Workouts";
 import GymPage from "./GymPage";
 import BodyWeightPage from "./BodyWeightPage";
-<<<<<<< Updated upstream
-=======
 import IntroCard from "./IntroCards";
 import ExerciseDetails from "./ExerciseDetail";
->>>>>>> Stashed changes
 
 const HomePage = () => {
   return (
@@ -18,21 +15,16 @@ const HomePage = () => {
       <Navbar />
       <MainNavbar />
       <ToggleNavbar />
-      <ul>
-        <img src="src/assets/Strong_Dobby.png" alt="Gym-Dobby" />
-        <img src="src/assets/Bellatrix.png" alt="Strong-Bellatrix" />
-      </ul>
-
+      <IntroCard />
       <Routes>
         <Route path="/workouts/bodyweight" element={<BodyWeightPage />} />
         <Route path="/workouts/bodyweight/:category" element={<BodyWeightPage />} />
         <Route path="/workouts/bodyweight/:category/:id" element={<ExerciseDetails />} />
         <Route path="/workouts/gym/:category" element={<GymPage />} />
-        <Route path="/workouts/gym" element={<GymPage />} />{" "}
+        <Route path="/workouts/gym" element={<GymPage />} />
         <Route path="/workouts/gym/:category/:id" element={<ExerciseDetails />} />
-        {/* Add this route to match /workouts/gym */}
-        <Route path="/workouts" element={<Workouts />} />
         <Route path="/workouts/:category" element={<Workouts />} />
+        <Route path="/workouts" element={<Workouts />} />
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </>
