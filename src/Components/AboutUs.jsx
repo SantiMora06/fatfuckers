04fe00c5f09c7b2827e97ac3d2f072bc55f25ebf
Santi Mera <santigomero@gmail.com>
2024-07-12 13:00:09 +0300

    One commit to go

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,8 +5,11 @@ const About = () => {
                 <div className="Omid">
                     <img src="src/assets/OMID.png" />
                     <h1>Omid Jahromi</h1>
-                    <p>On my late 20s, reservoir engineer, innovation passionate. Born and grown in Iran and currently based on Germany,
-                        with a strong programming and mathematical background click<a href="https://www.linkedin.com/in/omidesmaeelipoor/">HERE</a> </p>
+                    <p>and I am a doctoral researcher in the field of engineering from Iran.
+                        Currently, I am attending the Ironhack bootcamp to develop my skills and
+                        transition into a full-stack developer role. With a solid foundation in engineering
+                        and a passion for technology, I am excited to combine my research expertise with new
+                        programming and development skills. For more info click <a href="https://www.linkedin.com/in/omidesmaeelipoor/">HERE</a> </p>
                 </div>
                 <div className="Santi">
                     <img src="src/assets/file.jpeg" />
